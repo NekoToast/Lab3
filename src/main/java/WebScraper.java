94ca50cefbaf2adf400a85a25e3195a00d50160b
Łukasz Jaworski <luskus@o2.pl>
2019-03-08 21:35:19 +0100

    Small changes

--- src/main/java/WebScraper.java
+++ src/main/java/WebScraper.java
@@ -7,7 +7,7 @@ import java.io.IOException;
 public class WebScraper {
     public static void main(String[] args) throws IOException {
         String url = "http://www.filmweb.pl/ranking/netflix";
-        System.out.println(("Fetching from "+ url));
+        System.out.println(("Scrapping from "+ url));
 
         Document doc = Jsoup.connect(url).get();
         Elements filmLinks = doc.select("a.film__link");
