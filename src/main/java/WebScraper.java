204d7c8b88a126a76e46a2a30a0bf1820669c79c
Łukasz Jaworski <luskus@o2.pl>
2019-03-08 21:27:21 +0100

    Initial commit

---
+++ src/main/java/WebScraper.java
@@ -0,0 +1,21 @@
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
+
+import java.io.IOException;
+public class WebScraper {
+    public static void main(String[] args) throws IOException {
+        String url = "http://www.filmweb.pl/ranking/netflix";
+        System.out.println(("Fetching from "+ url));
+
+        Document doc = Jsoup.connect(url).get();
+        Elements filmLinks = doc.select("a.film__link");
+
+            for (Element link : filmLinks) {
+                System.out.println(" Film link:" + link.attr("abs:href") + ", film title: " + link.text());
+        }
+
+    }
+
+}
